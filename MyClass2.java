0d96c13c8e40b7ad7cd976fb238e763e9f62acf3
FleySA <137941325+FleySA@users.noreply.github.com>
2023-07-10 13:36:39 +0200

    Add files via upload

---
+++ MyClass2.java
@@ -0,0 +1,20 @@
+public class MyClass2 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = number1 + number2;
+        int number4 = number2 - number1;
+        int number5 = number2 / number1;
+        int number6 = number1 * number2;
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+
+
+        String text1 = ("Dobroe");
+        String text2 = ("Utro");
+        String text3 = ("Strana");
+        System.out.println("Dobroe" + "Utro" + "Strana");
+    }
+}
